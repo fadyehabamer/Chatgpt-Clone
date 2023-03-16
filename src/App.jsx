b45b047458e5586d2b7c31008f094aa43fb70777
fadyehabamer <fadyamer45@gmail.com>
2023-03-16 22:35:15 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import {
   TypingIndicator,
 } from '@chatscope/chat-ui-kit-react';
 
-const API_KEY = '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';
+const API_KEY = process.env.REACT_APP_API_KEY;
 // "Explain things like you would to a 10 year old learning how to code."
 const systemMessage = {
   //  Explain things like you're talking to a software professional with 5 years of experience.
